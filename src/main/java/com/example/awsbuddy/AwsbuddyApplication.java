5645c3dacf89bcb28b4860c5456aca5b991d2421
Devashish <devashishkhattar@gmail.com>
2018-03-29 10:27:28 +0530

    second commit

---
+++ src/main/java/com/example/awsbuddy/AwsbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.example.awsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsbuddyApplication.class, args);
+	}
+}
